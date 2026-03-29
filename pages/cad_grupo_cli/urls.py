2ea80117f7b8c1de2db54c0c1996435979360008
cralmad <cralmad@gmail.com>
2026-03-29 17:21:47 +0200

    cadgrupocli

--- pages/cad_grupo_cli/urls.py
+++ pages/cad_grupo_cli/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('grupocli/',      views.cad_grupo_cli_view, name='cadgrupocli'),
-    path('grupocli/cons',  views.cad_grupo_cli_cons_view, name='cadgrupocli_cons'),
+    path('',      views.cad_grupo_cli_view, name='cadgrupocli'),
+    path('cons',  views.cad_grupo_cli_cons_view, name='cadgrupocli_cons'),
 ]
